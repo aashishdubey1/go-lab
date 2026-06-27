670f69872cd25d2d3d6d65f63d88a282c4ae37ac
Aashish <aashishdubey.dev@gmail.com>
2026-06-27 19:05:28 +0530

    modified cli-greeter

--- modules/01-getting-started/cli-greeter/main.go
+++ modules/01-getting-started/cli-greeter/main.go
@@ -9,15 +9,21 @@ func main() {
 	var name string
 	var lang string 
 	
-	flag.StringVar(&name,"name","anon","enter your name")
-	flag.StringVar(&lang,"lang","en","language")
+	flag.StringVar(&name, "name", "anon", "enter your name")
+	flag.StringVar(&lang, "lang", "en", "language (en, fr, es)")
+
+
 	flag.Parse()
 	
-	if lang == "fr"{
-		fmt.Printf("Bonjour %s, comment tu vas \n",name)
-	} else if lang == "ger" {
-		fmt.Printf("Hallo %s, wie geht es dir \n",name)
-	}else {
+	switch lang {
+	case "en":
 		fmt.Printf("Hello %s, How are you \n",name)
+	case "fr":
+		fmt.Printf("Bonjour %s, comment tu vas \n",name)
+	case "ger":
+		fmt.Printf("Hallo %s, wie geht es dir \n",name)
+		default:
+		fmt.Printf("Unknown language: %s\n", lang)
 	}
+	
 }
